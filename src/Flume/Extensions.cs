5ace217861b46c69c6cd7cfd5696722615cdfe99
Rex Strowbridge <rex@actias.com>
2025-09-04 10:29:32 -0500

    - Updated how parameterless 'AddFlume' method works to include calling assembly by default if no assemblies are defined.
    - Added basic overloads

--- src/Flume/Extensions.cs
+++ src/Flume/Extensions.cs
@@ -1,6 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using Flume.Pipelines;
+using System.Reflection;
 using Flume.Registration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,12 +19,48 @@ public static class Extensions
     /// <returns>Service collection</returns>
     public static IServiceCollection AddFlume(this IServiceCollection services)
     {
-        // Register the mediator
-        services.AddScoped<IMediator, Mediator>();
-        services.AddScoped<ISender>(provider => provider.GetRequiredService<IMediator>());
-        services.AddScoped<IPublisher>(provider => provider.GetRequiredService<IMediator>());
-        
-        services.AddHandlers();
+        var assembly = Assembly.GetCallingAssembly();
+
+        services.AddFlume(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers handlers and mediator types from the specified assembly
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="assembly">Target assembly containing handlers</param>
+    /// <returns>Service collection</returns>
+    public static IServiceCollection AddFlume(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddFlume(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers handlers and mediator types from the specified assemblies
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="assemblies">Target assemblies containing handlers</param>
+    /// <returns>Service collection</returns>
+    public static IServiceCollection AddFlume(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+    {
+        services.AddFlume(cfg => cfg.RegisterServicesFromAssemblies(assemblies.ToArray()));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers handlers and mediator types by type
+    /// </summary>
+    /// <param name="services">Service collection</param>
+    /// <param name="type">The type used to determine the assembly to add.</param>
+    /// <returns>Service collection</returns>
+    public static IServiceCollection AddFlume(this IServiceCollection services, Type type)
+    {
+        services.AddFlume(cfg => cfg.RegisterServicesFromAssemblyContaining(type));
 
         return services;
     }
@@ -63,92 +100,5 @@ public static class Extensions
         ServiceRegistrar.AddRequiredServices(services, configuration);
 
         return services;
-    }
-
-    private static void AddHandlers(this IServiceCollection services, System.Reflection.Assembly[]? assemblies = null)
-    {
-        var assembliesToScan = assemblies ?? [System.Reflection.Assembly.GetCallingAssembly()];
-
-        foreach (var assembly in assembliesToScan)
-        {
-            // Register request handlers
-            var requestHandlerTypes = assembly.GetTypes()
-                .Where(t => t is { IsAbstract: false, IsInterface: false } &&
-                            t.GetInterfaces().Any(i => i.IsGenericType
-                                                       && (i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)
-                                                           || i.GetGenericTypeDefinition() == typeof(IRequestHandler<>)))
-                );
-
-            foreach (var handlerType in requestHandlerTypes)
-            {
-                var interfaces = handlerType
-                    .GetInterfaces()
-                    .Where(i => i.IsGenericType
-                                && (i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)
-                                    || i.GetGenericTypeDefinition() == typeof(IRequestHandler<>))
-                    );
-
-                foreach (var interfaceType in interfaces)
-                {
-                    services.AddScoped(interfaceType, handlerType);
-                }
-            }
-
-            // Register notification handlers
-            var notificationHandlerTypes = assembly
-                .GetTypes()
-                .Where(t => t is { IsAbstract: false, IsInterface: false }
-                            && t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>))
-                );
-
-            foreach (var handlerType in notificationHandlerTypes)
-            {
-                var interfaces = handlerType.GetInterfaces()
-                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(INotificationHandler<>));
-
-                foreach (var interfaceType in interfaces)
-                {
-                    services.AddScoped(interfaceType, handlerType);
-                }
-            }
-
-            // Register stream request handlers
-            var streamRequestHandlerTypes = assembly
-                .GetTypes()
-                .Where(t => t is { IsAbstract: false, IsInterface: false }
-                            && t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IStreamRequestHandler<,>))
-                );
-
-            foreach (var handlerType in streamRequestHandlerTypes)
-            {
-                var interfaces = handlerType.GetInterfaces()
-                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IStreamRequestHandler<,>));
-
-                foreach (var interfaceType in interfaces)
-                {
-                    services.AddScoped(interfaceType, handlerType);
-                }
-            }
-
-            // Register pipeline behaviors
-            var pipelineBehaviorTypes = assembly
-                .GetTypes()
-                .Where(t => t is { IsAbstract: false, IsInterface: false }
-                            && t.GetInterfaces()
-                                .Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IPipelineBehavior<,>))
-                );
-
-            foreach (var behaviorType in pipelineBehaviorTypes)
-            {
-                var interfaces = behaviorType
-                    .GetInterfaces()
-                    .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IPipelineBehavior<,>));
-
-                foreach (var interfaceType in interfaces)
-                {
-                    services.AddScoped(interfaceType, behaviorType);
-                }
-            }
-        }
     }
 }
