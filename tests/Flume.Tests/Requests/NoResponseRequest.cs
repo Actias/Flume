4ef40e22ce3d878a14003961c852a10eea3dbbe1
Rex Strowbridge <rex@actias.com>
2025-09-15 19:35:11 -0500

    Adding unit test to test for NoResponse issue.

---
+++ tests/Flume.Tests/Requests/NoResponseRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flume.Tests.Requests;
+
+internal sealed record NoResponseRequest : IRequest;
+
+internal sealed class NoResponseRequestHandler : IRequestHandler<NoResponseRequest>
+{
+    public Task Handle(NoResponseRequest request, CancellationToken cancellationToken = default)
+    {
+        // Intentionally does nothing and returns a completed task
+        return Task.CompletedTask;
+    }
+}
